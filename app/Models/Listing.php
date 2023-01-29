3724cdbd509c3eac1ea8d590bedbdb7c1708b86c
Sahid Bismantoko <sahid.bismantoko@gmail.com>
2023-01-29 13:59:12 +0700

    Create model Listing

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'listing one',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, aliquid?',
-            ],
-            [
-                'id' => 2,
-                'title' => 'listing two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, aliquid?',
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id)
-    {
-        $listings = self::all();
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }

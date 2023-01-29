bab3e939a3d1ab8bdbd0623cabfb1b4f9b69c54f
Sahid Bismantoko <sahid.bismantoko@gmail.com>
2023-01-29 21:08:22 +0700

    adding template

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listing</title>
-</head>
-
-<body>
-    <h2>{{$listing['title']}}</h2>
-    <p>{{$listing['description']}}</p>
-</body>
-
-</html>
+@extends('layouts.main')
+@section('content')
+<h2>{{$listing['title']}}</h2>
+<p>{{$listing['description']}}</p>
+@endsection

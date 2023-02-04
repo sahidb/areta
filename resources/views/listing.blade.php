b98e0cba7e49d3322c985ac76e9edf149bdfa183
Sahid Bismantoko <sahid.bismantoko@gmail.com>
2023-02-04 13:21:37 +0700

    x-card class merge

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -4,7 +4,7 @@
 <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-    <x-card>
+    <x-card class="p-10">
         <div class="flex flex-col items-center justify-center text-center">
             <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt="" />
 

a9a5e95300106b5a3b6f9a6d2ea5bcc2b74ae37c
Sahid Bismantoko <sahid.bismantoko@gmail.com>
2023-02-04 21:52:49 +0700

    register form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -44,6 +45,11 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register User
+Route::get('/register', [UserController::class, 'create']);
+
+
 
 // Route::get('/listings/{id}', function ($id) {
 //     $listing = Listing::find($id);
